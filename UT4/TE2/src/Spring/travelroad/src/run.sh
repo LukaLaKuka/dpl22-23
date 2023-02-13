1bf3a86148362a149aba3f6772c61d97f21c8d3a
Tomhuel <tomasnahuelantela@gmail.com>
2023-02-13 20:35:43 +0000

    por si acaso

--- UT4/TE2/src/Spring/travelroad/src/run.sh
+++ UT4/TE2/src/Spring/travelroad/src/run.sh
@@ -2,11 +2,13 @@
 
 # Como este fichero lo guardaré en travelroad/src/run.sh por lo
 # tengo que salir solamente hasta el nivel de travelroad
+cd $(dirname $0)
+
 cd ..
 
 ./mvnw package
 
-JAR = `ls target/*.jar -t | head -1`
+JAR= `ls target/*.jar -t | head -1`
 
 /usr/bin/java -jar $JAR
 
